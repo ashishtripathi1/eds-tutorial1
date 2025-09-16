cb90dad2799ee39da0315d158908901912edf23c
ashishtripathi1 <ashishtripathi1@yahoo.co.in>
2025-09-16 12:12:16 +0530

    Update apidemo.js

--- blocks/apidemo/apidemo.js
+++ blocks/apidemo/apidemo.js
@@ -3,11 +3,11 @@ import {
 } from '../../scripts/aem.js';
 
 export default async function decorate(block) {
-	fetchData(block){
-		const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
-  .then(response => response.clone.json())
- 	block.append(response.title);
- 	console.log(response);
+	//fetchData(block){
+		//const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
+ // .then(response => response.clone.json())
+ 	//block.append("This is printed here");
+ 	console.log("This is printed here");
   
   }
  }
