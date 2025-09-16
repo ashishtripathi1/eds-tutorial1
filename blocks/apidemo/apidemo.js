b130a2fe05231d0b88a7e0c2bde4488aa33c09e8
ashishtripathi1 <ashishtripathi1@yahoo.co.in>
2025-09-16 13:32:20 +0530

    Update apidemo.js

--- blocks/apidemo/apidemo.js
+++ blocks/apidemo/apidemo.js
@@ -1,10 +1,10 @@
 
 export default async function decorate(block) {
-	fetchData(block){
+	fetch(block){
 		const response = await fetch('https://jsonplaceholder.typicode.com/posts/1')
   .then(response => response.clone.json())
  	block.append("This is printed here");
  	console.log("This is printed here");
-  
+	}
   }
  
